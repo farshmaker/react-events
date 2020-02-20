ef96096a6564574942a4aa189fd9b2d99a78c1d0
farshmaker <killenbhv@gmail.com>
2020-02-20 12:52:59 +0300

    simple static jsx

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("test")
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const el = (
+    <div>
+        <h1>Event List</h1>
+        <input placeholder="search" />
+        <ul>
+            <li>Mozgo</li>
+            <li>Lazertag</li>
+        </ul>
+    </div>
+);
+
+ReactDOM.render(el, document.getElementById('root'));
